dd42724c51046c95282e3a8fce65e044af6e9385
AbroShahzeb <khizr772@gmail.com>
2025-04-30 17:24:03 +0500

    Added deployment setup

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || 3010, () => {
+  console.log("Server is running on port 3010");
+});
