e4ef5240402bee01af2587db975b0682be328e70
AbroShahzeb <khizr772@gmail.com>
2025-04-30 17:38:35 +0500

    Implemented global error handling and connected to DB

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
+import "colors";
+import globalErrorHandler from "./controllers/error.controller.js";
+import AppError from "./utils/appError.js";
+import { connectDB } from "./utils/db.js";
 
 const app = express();
 
+connectDB();
+
 app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World! HEheehehiehwi");
 });
+
+app.get("/error", (req, res, next) => {
+  return next(new AppError("An annoying error occurred", 400));
+});
+
+app.use(globalErrorHandler);
 
 app.listen(process.env.PORT || 3010, () => {
   console.log("Server is running on port 3010, hehe");
