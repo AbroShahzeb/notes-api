b8b0af6d0e528c8d202e37503b8e3c0fc89d8ae2
AbroShahzeb <khizr772@gmail.com>
2025-04-30 17:30:46 +0500

    Deployment test

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! HEheehehiehwi");
 });
 
 app.listen(process.env.PORT || 3010, () => {
-  console.log("Server is running on port 3010");
+  console.log("Server is running on port 3010, hehe");
 });
